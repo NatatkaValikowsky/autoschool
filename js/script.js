8497e50007ae3bc6a980043834ccd355f4c49b36
Natalya Valikova <valikowa.natalya@yandex.ru>
2023-03-30 21:54:23 +0900

    верстка

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll(".question:not(.question--empty)").forEach(function(el){
+      el.addEventListener("click", function (){
+          el.classList.toggle("question--opened");
+      });
+  });   
+});
+
 $(document).ready(function(){
     $('.advantages-slider').slick({
         slidesToShow: 1,
@@ -10,6 +18,36 @@ $(document).ready(function(){
         arrows: true,
         prevArrow: '.advantages-arrow-left',
         nextArrow: '.advantages-arrow-right',
+        responsive: [
+            {
+              breakpoint: 1100,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                arrows: true,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true
+              }
+            }
+          ]
+      });
+
+      $('.autopark-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: true,
+        dots: true,
+        appendDots: ".autopark-dots",
+        arrows: false,
         responsive: [
             {
               breakpoint: 1100,
